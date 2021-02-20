1ab322f9271a246ab16677f7a4403c46d59a08df
Phi Van Ngoc <ngocketit@gmail.com>
2021-02-20 16:47:17 +0200

    Update providers

--- providers.tf
+++ providers.tf
@@ -1,20 +1,25 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    aws = {
+      version = ">= 3.0"
+    }
+}
+
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-  version = "~> 2.0"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 provider "null" {
-  version = "1.0.0"
 }
 
 provider "template" {
-  version = "1.0.0"
 }
 
 provider "github" {
-  version = "1.3.0"
-  token        = "${var.github_oauth_token}"
-  organization = "${var.github_organization}"
+  token        = var.github_oauth_token
+  organization = var.github_organization
 }
